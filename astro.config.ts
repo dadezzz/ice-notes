70d003d96d66f1c723f36c16633510c1a1ebb90c
Davide Zarantonello <davide@zarantonello.dev>
2025-11-16 15:18:32 +0000

    feat: catch up fisica and fix some errors (#194)

--- astro.config.ts
+++ astro.config.ts
@@ -48,22 +48,25 @@ export default defineConfig({
             "\\Z0": "Z_0\\left[#1\\right]", // Response of a circuit.
             "\\arg": "\\text{arg}\\left(#1\\right)", // Argument of a complex number.
             // Various functions.
-            "\\fRectangle": "\\Pi\\left(#1\\right)", // Rectangle from signal processing.
-            "\\fThriangle": "\\Lambda\\left(#1\\right)", // Thriangle from signal processing.
-            "\\fSign": "s\\left(#1\\right)", // Sign function.
-            "\\fStep": "\\theta\\left(#1\\right)", // Step function.
-            "\\fDelta": "\\delta\\left(#1\\right)", // Dirac delta function.
-            "\\fLog": "\\text{log}\\left(#1\\right)", // Logarithm.
-            "\\fLn": "\\text{log}\\left(#1\\right)", // Natural logarithm.
-            "\\fP": "\\mathbb{P}\\left(#1\\right)", // Probability.
+            "\\fRectangle": "\\Pi\\!\\left(#1\\right)", // Rectangle from signal processing.
+            "\\fThriangle": "\\Lambda\\!\\left(#1\\right)", // Thriangle from signal processing.
+            "\\fSign": "s\\!\\left(#1\\right)", // Sign function.
+            "\\fStep": "\\theta\\!\\left(#1\\right)", // Step function.
+            "\\fDelta": "\\delta\\!\\left(#1\\right)", // Dirac delta function.
+            "\\fLog": "\\text{log}\\!\\left(#1\\right)", // Logarithm.
+            "\\fLn": "\\text{log}\\!\\left(#1\\right)", // Natural logarithm.
+            "\\fP": "\\mathbb{P}\\!\\left(#1\\right)", // Probability.
             // Angular functions with adaptive parentheses.
-            "\\fArctan": "\\text{arctan}\\left(#1\\right)",
-            "\\fCos": "\\text{cos}\\left(#1\\right)",
-            "\\fSinc": "\\text{sinc}\\left(#1\\right)",
-            "\\fSin": "\\text{sin}\\left(#1\\right)",
-            "\\fTan": "\\text{tan}\\left(#1\\right)",
+            "\\fArctan": "\\text{arctan}\\!\\left(#1\\right)",
+            "\\fCos": "\\text{cos}\\!\\left(#1\\right)",
+            "\\fSinc": "\\text{sinc}\\!\\left(#1\\right)",
+            "\\fSin": "\\text{sin}\\!\\left(#1\\right)",
+            "\\fTan": "\\text{tan}\\!\\left(#1\\right)",
             // Transform notation with double arrow.
-            "\\trFourier": "\\xleftrightarrow{\\mathcal{F}}",
+            "\\trFourierA": "\\xleftrightarrow{\\mathcal{F}}",
+            "\\trFourierB": "\\mathcal{F}\\left\\{#1\\right\\}",
+            "\\trLaplaceA": "\\xleftrightarrow{\\mathcal{L}}",
+            "\\trLaplaceB": "\\mathcal{L}\\left\\{#1\\right\\}",
           },
         },
       ],
